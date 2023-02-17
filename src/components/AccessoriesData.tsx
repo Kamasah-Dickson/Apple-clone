254cd7ccfd579939a42b83929511537898c25e3b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-17 02:50:44 +0000

    add accessories data

---
+++ src/components/AccessoriesData.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AccessoriesData({ Navdata }: any) {
+	const title = Navdata.title;
+	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const explore_accessories_title = Navdata?.explore_accessories_title;
+	const explore_accessories_links = Navdata.explore_accessories_links?.map(
+		(data: any) => <li key={data}>{data}</li>
+	);
+
+	return (
+		<div className="my-grid mt-8 max-w-[400px]">
+			<div>
+				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
+				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+					{list}
+				</ul>
+			</div>
+			<div className="justify-self-end">
+				<h3 className="text-[12px] text-[#86868b]">
+					{explore_accessories_title}
+				</h3>
+				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+					{explore_accessories_links}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default AccessoriesData;
