d94a8ed44577e44983a90e4af0b426ba96f9cc3c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-18 02:48:14 +0000

    add swiperData

---
+++ src/swiperData.ts
@@ -0,0 +1,57 @@
+import helloTomorrow from "./assets/slider-images/hello-tomorrow.jpg";
+import shrinking from "./assets/slider-images/shrinking.jpg";
+import ted_lasso from "./assets/slider-images/ted-lasso.jpg";
+import dear_edward from "./assets/slider-images/dear-edward.jpg";
+import truthBetold from "./assets/slider-images/truth-be-told.jpg";
+import servant from "./assets/slider-images/servant.jpg";
+import causeway from "./assets/slider-images/causeway.jpg";
+import severance from "./assets/slider-images/severance.jpg";
+import blackBird from "./assets/slider-images/black-bird.jpg";
+
+export const swiperData = [
+	{
+		img: helloTomorrow,
+		genre: "Drama",
+		text: "Shoot for the moon",
+	},
+	{
+		img: shrinking,
+		genre: "Comedy",
+		text: "Breakdown.Breakthrough.",
+	},
+	{
+		img: ted_lasso,
+		genre: "Comedy",
+		text: "Catch up before Season 3 premieres on March 15.",
+	},
+	{
+		img: dear_edward,
+		genre: "Drama",
+		text: "Connie Britton stars in a new series from the producer of Friday Night Lights.",
+	},
+	{
+		img: truthBetold,
+		genre: "Drama",
+		text: "No disappearance is a coincidence.",
+	},
+	{
+		img: servant,
+		genre: "Thriller",
+		text: "The final reckoning",
+	},
+	{
+		img: causeway,
+		genre: "Drama",
+		text: "Oscar® Nominee: Best Actor in a Supporting Role.",
+	},
+	{
+		img: severance,
+		genre: "Thriller",
+		text: "We'er all different people at work",
+	},
+	{
+		img: blackBird,
+		genre: "Drama",
+		text: "One man's freedom hides in another man's darkness",
+	},
+];
