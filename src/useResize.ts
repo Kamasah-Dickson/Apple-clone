3a078141bb77416ac27576c3f61f8b2bd81c876c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-18 02:47:08 +0000

    add useResize

---
+++ src/useResize.ts
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function useResize() {
+	const [size, setSize] = useState(window.innerWidth);
+
+	function checkResize() {
+		setSize(window.innerWidth);
+	}
+	useEffect(() => {
+		window.addEventListener("resize", checkResize);
+		return () => window.removeEventListener("resize", checkResize);
+	}, [size]);
+
+	return { size };
+}
+
+export default useResize;
