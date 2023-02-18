eb81966fab5ff27b818e4ba41afe08f535c63704
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-18 02:48:56 +0000

    add commerce data

---
+++ src/commerceGridData.ts
@@ -0,0 +1,50 @@
+import seriesWatch from "./assets/serieswatch.jpg";
+import iPad from "./assets/iPad.jpg";
+import macbookPro from "./assets/macbookPro.jpg";
+import homePod from "./assets/homePod.jpg";
+import macDoes from "./assets/macDoes.jpg";
+import iCard from "./assets/iCard.jpg";
+import logo from "./assets/logo.svg";
+export const gridData = [
+	{
+		img: seriesWatch,
+		h1: "WATCH",
+		logo: logo,
+		p: "A healthy leap ahead.",
+		span: "SERIES 8",
+		white: true,
+		imgShow: true,
+	},
+	{
+		img: iPad,
+		h1: "iPad",
+		p: "Lovable. Drawable. Magical.",
+		white: false,
+	},
+	{
+		img: macbookPro,
+		h1: "MacBookPro",
+		p: "Supercharged by M2 Pro and M2 Max.",
+		white: true,
+	},
+	{
+		img: homePod,
+		h1: "HomePod",
+		p: "Profound sound",
+		white: true,
+	},
+	{
+		img: macDoes,
+		h1: "Mac does that.",
+		p: "Discover what Mac can do for you",
+		white: false,
+		more: true,
+		green: true,
+	},
+	{
+		img: iCard,
+		h1: "Card",
+		p: "Get up to 3% Daily cash back with every purchase.",
+		white: false,
+	},
+];
