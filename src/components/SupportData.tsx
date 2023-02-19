ba06cd35d8e13d91dc4253edde221f28cb427c68
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-19 03:37:00 +0000

    update support

--- src/components/SupportData.tsx
+++ src/components/SupportData.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function SupportData({ Navdata }: any) {
 	const title = Navdata.title;
 	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
@@ -13,20 +11,20 @@ function SupportData({ Navdata }: any) {
 	);
 
 	return (
-		<div className="my-grid">
+		<div className="my-grid mx-[20px] mt-[150px] h-screen md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
 				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
 					{list}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{getHelp_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{getHelp_links}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{helpful_topics_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{helpful_topics_links}
