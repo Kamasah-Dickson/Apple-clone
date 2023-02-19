400a992740fd74429dc53368546b2d4c4513e92c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-19 03:44:20 +0000

    update swiperDataa

--- src/swiperData.ts
+++ src/swiperData.ts
@@ -1,56 +1,92 @@
 import helloTomorrow from "./assets/slider-images/hello-tomorrow.jpg";
+import helloTomorrowMobile from "./assets/slider-images/mobile/hello-tomorrow-mobile.jpg";
+import helloTomorrowMobile_name from "./assets/slider-images/mobile/hello-name.png";
 import shrinking from "./assets/slider-images/shrinking.jpg";
+import shrinkinMobile from "./assets/slider-images/mobile/shrinking-mobile.jpg";
+import shrinkinMobile_name from "./assets/slider-images/mobile/shrinking-name-mobile.png";
 import ted_lasso from "./assets/slider-images/ted-lasso.jpg";
+import ted_lass_mobile from "./assets/slider-images/mobile/ted-lasso-mobile.jpg";
+import ted_lass_mobile_name from "./assets/slider-images/mobile/ted-lasso-name.png";
 import dear_edward from "./assets/slider-images/dear-edward.jpg";
+import dear_edward_mobile from "./assets/slider-images/mobile/dear-edward-mobile.jpg";
+import dear_edward_mobile_name from "./assets/slider-images/mobile/dear-edward-name.png";
 import truthBetold from "./assets/slider-images/truth-be-told.jpg";
+import truthBetold_mobile from "./assets/slider-images/mobile/truth-mobile.jpg";
+import truthBetold_mobile_name from "./assets/slider-images/mobile/truth-name.png";
 import servant from "./assets/slider-images/servant.jpg";
+import servant_mobile from "./assets/slider-images/mobile/servant-mobile.jpg";
+import servant_mobile_name from "./assets/slider-images/mobile/servant-name.png";
 import causeway from "./assets/slider-images/causeway.jpg";
+import causeway_mobile from "./assets/slider-images/mobile/causeway-mobile.jpg";
+import causeway_mobile_name from "./assets/slider-images/mobile/causeway-name.png";
 import severance from "./assets/slider-images/severance.jpg";
+import severance_mobile from "./assets/slider-images/mobile/servant-mobile.jpg";
+import severance_mobile_name from "./assets/slider-images/mobile/servant-name.png";
 import blackBird from "./assets/slider-images/black-bird.jpg";
+import blackBird_mobile from "./assets/slider-images/mobile/blackbird-mobile.jpg";
+import blackBird_mobile_name from "./assets/slider-images/mobile/blackbird-mobile-name.png";
 
 export const swiperData = [
 	{
 		img: helloTomorrow,
+		mobile_img: helloTomorrowMobile,
+		mobile_name: helloTomorrowMobile_name,
 		genre: "Drama",
 		text: "Shoot for the moon",
 	},
 	{
 		img: shrinking,
+		mobile_img: shrinkinMobile,
+		mobile_name: shrinkinMobile_name,
 		genre: "Comedy",
 		text: "Breakdown.Breakthrough.",
 	},
 	{
 		img: ted_lasso,
+		mobile_img: ted_lass_mobile,
+		mobile_name: ted_lass_mobile_name,
 		genre: "Comedy",
 		text: "Catch up before Season 3 premieres on March 15.",
 	},
 	{
 		img: dear_edward,
+		mobile_img: dear_edward_mobile,
+		mobile_name: dear_edward_mobile_name,
 		genre: "Drama",
 		text: "Connie Britton stars in a new series from the producer of Friday Night Lights.",
 	},
 	{
 		img: truthBetold,
+		mobile_img: truthBetold_mobile,
+		mobile_name: truthBetold_mobile_name,
 		genre: "Drama",
 		text: "No disappearance is a coincidence.",
 	},
 	{
 		img: servant,
+		mobile_img: servant_mobile,
+		mobile_name: servant_mobile_name,
 		genre: "Thriller",
 		text: "The final reckoning",
 	},
 	{
 		img: causeway,
+		mobile_img: causeway_mobile,
+		mobile_name: causeway_mobile_name,
 		genre: "Drama",
 		text: "Oscar® Nominee: Best Actor in a Supporting Role.",
 	},
 	{
 		img: severance,
+		mobile_img: severance_mobile,
+		mobile_name: severance_mobile_name,
 		genre: "Thriller",
 		text: "We'er all different people at work",
 	},
 	{
 		img: blackBird,
+		mobile_img: blackBird_mobile,
+		mobile_name: blackBird_mobile_name,
 		genre: "Drama",
 		text: "One man's freedom hides in another man's darkness",
 	},
