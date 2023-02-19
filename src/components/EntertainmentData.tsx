07be7d8b0b40a5e52abeff1465e5df0a70212f35
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-19 03:39:33 +0000

    update entertainment

--- src/components/EntertainmentData.tsx
+++ src/components/EntertainmentData.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function EntertainmentData({ Navdata }: any) {
 	const title = Navdata.title;
 	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
@@ -9,14 +7,14 @@ function EntertainmentData({ Navdata }: any) {
 	));
 
 	return (
-		<div className="my-grid mt-8 max-w-[400px]">
+		<div className="my-grid mx-[20px] mt-[150px] h-screen max-w-[400px] md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
 				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
 					{list}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{entertainment_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{entertainment_links}
