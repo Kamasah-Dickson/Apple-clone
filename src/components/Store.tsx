42d86533bc71f08b1fa9911c974be508ca548bc0
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-19 03:38:59 +0000

    udate store

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Store() {
 	return (
 		<main>
--- src/components/StoreData.tsx
+++ src/components/StoreData.tsx
@@ -21,20 +21,20 @@ function StoreData({ Navdata }: any) {
 	));
 
 	return (
-		<div className="my-grid">
+		<div className="my-grid mx-[20px] mt-[150px] h-screen md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
 				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
 					{list}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{quickLinks_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{quickLinks}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="mt-5 md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{specials_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{specialLinks}
