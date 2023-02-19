66102d6b33bf2d7832fb4b023ca21c191d6cae09
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-19 03:40:57 +0000

    update tv&home

--- src/components/Tv&homeData.tsx
+++ src/components/Tv&homeData.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Tv_homeData({ Navdata }: any) {
 	const title = Navdata.title;
 	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
@@ -13,20 +11,20 @@ function Tv_homeData({ Navdata }: any) {
 	));
 
 	return (
-		<div className="my-grid">
+		<div className="my-grid mx-[20px] mt-[150px] h-screen md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
 				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
 					{list}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{shopTV_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{shopTV_links}
 				</ul>
 			</div>
-			<div className="justify-self-end">
+			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{moreTV_title}</h3>
 				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
 					{moreTV_links}
