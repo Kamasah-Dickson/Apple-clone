593cf9475eb7f97b4f2746ef5c531f1f2a215f6b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-20 20:06:10 +0000

    add page not found

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+	return (
+		<div className="grid h-screen w-full place-content-center text-2xl font-medium text-black">
+			Page Not constructed 🚧
+		</div>
+	);
+}
+
+export default PageNotFound;
