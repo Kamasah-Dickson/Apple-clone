ca16fd8d9789c88c073cd8b43f9e7dfb72018617
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-21 03:11:28 +0000

    add Top add

---
+++ src/components/TopAdd.tsx
@@ -0,0 +1,15 @@
+function TopAdd() {
+	return (
+		<div className="bg-white py-1">
+			<p className="mx-auto max-w-[700px] py-2 text-center text-[12px] font-normal md:text-[15px]">
+				Get $200–$570 in credit toward an iPhone 14 when you trade in an iPhone
+				11 or newer.
+				<a className="font-semibold text-[#2997ff] " href="#">
+					<sup>1</sup> Shop iPhone
+				</a>
+			</p>
+		</div>
+	);
+}
+
+export default TopAdd;
