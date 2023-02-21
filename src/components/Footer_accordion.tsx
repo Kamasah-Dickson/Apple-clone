797dd12a11b70a284eb97f3ec99c05163684af0a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-21 03:09:46 +0000

    update footer on mobile

--- src/components/Footer_accordion.tsx
+++ src/components/Footer_accordion.tsx
@@ -1,226 +1,158 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import FAQItem from "./FooterItem";
+
+const shopAndLearn = {
+	links: [
+		"Store",
+		"Mac",
+		"iPad",
+		"iPhone",
+		"Watch",
+		"Airpods",
+		"TV & Home",
+		"AirTag",
+		"Accessories",
+		"Gift Cards",
+	],
+	title: "Shop and Learn",
+};
+
+const AppleWallet = {
+	title: "Apple Wallet",
+	links: ["Apple Card", "Apple Pay", "Apple Cash"],
+	listHeight90: true,
+};
+
+const manageId = {
+	title: "Manage Your Apple ID",
+	links: ["Manage Your Apple ID", "Apple Store Account", "iCloud.com"],
+	listHeight90: true,
+};
+
+const entertainment = {
+	title: "Entertainment",
+	links: [
+		"Apple One",
+		"Apple TV+",
+		"Apple Music",
+		"Apple Arcade",
+		"Apple Fitness+",
+		"Apple News+",
+		"Apple Podcasts",
+		"Apple Books",
+		"Apple Store",
+	],
+};
+const appleStore = {
+	title: "Apple Store",
+	links: [
+		"Find a Store",
+		"Genius Bar",
+		"Today at Apple",
+		"Apple Camp",
+		"Apple Store App",
+		"Certified Refurbished",
+		"Financing",
+		"Apple Trade In",
+		"Order Status",
+		"Shopping Help",
+	],
+};
+
+const business = {
+	title: "For Business",
+	links: ["Find a Store", "Apple and Business", "Shop for Business"],
+	listHeight90: true,
+};
+const education = {
+	title: "For Education",
+	links: ["Apple and Education", "Shop for K-12", "Shop for College"],
+	listHeight90: true,
+};
+
+const healthCare = {
+	title: "For Healthcare",
+	links: [
+		"Apple in Healthcare",
+		"Health on Apple Watch",
+		"Health Records on iPhone",
+	],
+	listHeight90: true,
+};
+const government = {
+	title: "For Government",
+	links: ["Shop for Government", "Shop for Veterans and Military"],
+	listHeight90: true,
+};
+
+const appleValues = {
+	title: "Apple Values",
+	links: [
+		"Accessibility",
+		"Education",
+		"Environment",
+		"Inclusion and Diversity",
+		"Privacy",
+		"Racial Equity and Justice",
+		"Supplier Responsibility",
+	],
+};
+
+const aboutApple = {
+	title: "About Apple",
+	links: [
+		"Apple Leadership",
+		"Career Opportunities",
+		"Investors",
+		"Ethics and Compliance",
+		"Events",
+		"Contact Apple",
+	],
+};
 
 function Footer_accordion() {
-	const [show, setShow] = useState(false);
 	return (
 		<div className="pt-2">
 			<div className="">
 				<div className="flex flex-col gap-3">
-					<div
-						className=" my-hr cursor-pointer pt-3 pb-1"
-						onClick={() => setShow((prev) => !prev)}
-					>
-						<h3 className="flex items-center justify-between text-[14px] font-medium">
-							<span className="text-[#1d1d1f]">Shop and Learn</span>
-							<button>+</button>
-						</h3>
-						<ul
-							className={` my-trans flex ${
-								show ? "h-[270px]" : "h-0"
-							} flex-col justify-center gap-2 overflow-hidden text-[12px] text-[#6e6e73]`}
-						>
-							<Link className="" to={"/store"}>
-								Store
-							</Link>
-							<Link to={"/mac"}>Mac</Link>
-							<Link to={"/iPad"}>iPad</Link>
-							<Link to={"/iPhone"}>iPhone</Link>
-							<Link to={"/watch"}>Watch</Link>
-							<Link to={"/airpods"}>Airpods</Link>
-							<Link to={"/tv&home"}>TV & Home</Link>
-							<Link to={"/airtag"}>AirTag</Link>
-							<Link to={"/accessories"}>Accessories</Link>
-							<Link to={"/giftcards"}>Gift Cards</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">Apple Wallet</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/Wallet"}>
-								Wallet
-							</Link>
-							<Link to={"/applecard"}>Apple Card</Link>
-							<Link to={"/Applepay"}>Apple Pay</Link>
-							<Link to={"/Applecash"}>Apple Cash</Link>
-						</ul>
-					</div>
-				</div>
-				<div className="flex flex-col gap-3">
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">Account</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/appleID"}>
-								Manage Your Apple ID
-							</Link>
-							<Link to={"/appleaccount"}>Apple Store Account</Link>
-							<Link to={"/iCloud"}>iCloud.com</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">Entertainment</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/appleone"}>
-								Apple One
-							</Link>
-							<Link to={"/appletv"}>Apple TV+</Link>
-							<Link to={"/applemusic"}>Apple Music</Link>
-							<Link to={"/applearcade"}>Apple Arcade</Link>
-							<Link to={"/applefitness+"}>Apple Fitness+</Link>
-							<Link to={"/applenews+"}>Apple News+</Link>
-							<Link to={"/applepodcasts"}>Apple Podcasts</Link>
-							<Link to={"/applebooks"}>Apple Books</Link>
-							<Link to={"/applestore"}>Apple Store</Link>
-						</ul>
-					</div>
-				</div>
-				<div>
-					<h3 className="mb-2 mt-4 text-[12px] font-medium">
-						<span className="text-[#1d1d1f]">Apple Store</span>
-						<button className="hidden" disabled>
-							+
-						</button>
-					</h3>
-					<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-						<Link className="" to={"/findastore"}>
-							Find a Store
-						</Link>
-						<Link to={"/geniusbar"}>Genius Bar</Link>
-						<Link to={"/todayatapple"}>Today at Apple</Link>
-						<Link to={"/applecamp"}>Apple Camp</Link>
-						<Link to={"/applestoreapp"}>Apple Store App</Link>
-						<Link to={"/certifiedrefurbished"}>Certified Refurbished</Link>
-						<Link to={"/financing"}>Financing</Link>
-						<Link to={"/appletradein"}>Apple Trade In</Link>
-						<Link to={"/orderstatus"}>Order Status</Link>
-						<Link to={"/shoppinghelp"}>Shopping Help</Link>
-					</ul>
-				</div>
-				<div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">For Business</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/findastore"}>
-								Find a Store
-							</Link>
-							<Link to={"/appleandbusiness"}>Apple and Business</Link>
-							<Link to={"/shopforbusiness"}>Shop for Business</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">For Education</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/appleandeducation"}>
-								Apple and Education
-							</Link>
-							<Link to={"/shopk-12"}>Shop for K-12</Link>
-							<Link to={"/shopforcollege"}>Shop for College</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">For Healthcare</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/appleinhealthcare"}>
-								Apple in Healthcare
-							</Link>
-							<Link to={"/healthonapplewatch"}>Health on Apple Watch</Link>
-							<Link to={"/healthrecordsonphone"}>Health Records on iPhone</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">For Government</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/shopforgovernment"}>
-								Shop for Government
-							</Link>
-							<Link to={"/veterans and Military"}>
-								Shop for Veterans and Military
-							</Link>
-						</ul>
-					</div>
-				</div>
-				<div className="flex flex-col gap-3">
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">Apple Values</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/accessibility"}>
-								Accessibility
-							</Link>
-							<Link to={"/education"}>Education</Link>
-							<Link to={"/environment"}>Environment</Link>
-							<Link to={"/inclusionandiversity"}>Inclusion and Diversity</Link>
-							<Link to={"/privacy"}>Privacy</Link>
-							<Link to={"/racialequityandjustice"}>
-								Racial Equity and Justice
-							</Link>
-							<Link to={"/supplierresponsibility"}>
-								Supplier Responsibility
-							</Link>
-						</ul>
-					</div>
-					<div>
-						<h3 className="mb-2 mt-4 text-[12px] font-medium">
-							<span className="text-[#1d1d1f]">About Apple</span>
-							<button className="hidden" disabled>
-								+
-							</button>
-						</h3>
-						<ul className="flex flex-col justify-center gap-2 text-[12px] text-[#6e6e73]">
-							<Link className="" to={"/newsroom"}>
-								Newsroom
-							</Link>
-							<Link to={"/appleleadership"}>Apple Leadership</Link>
-							<Link to={"/careeropportunities"}>Career Opportunities</Link>
-							<Link to={"/investors"}>Investors</Link>
-							<Link to={"/Ethics&Compliance"}>Ethics and Compliance</Link>
-							<Link to={"/Events"}>Events</Link>
-							<Link to={"/contactapple"}>Contact Apple</Link>
-						</ul>
-					</div>
+					<FAQItem links={shopAndLearn.links} title={shopAndLearn.title} />
+					<FAQItem
+						links={AppleWallet.links}
+						title={AppleWallet.title}
+						listHeight90={AppleWallet.listHeight90}
+					/>
+					<FAQItem
+						links={manageId.links}
+						title={manageId.title}
+						listHeight90={manageId.listHeight90}
+					/>
+					<FAQItem links={entertainment.links} title={entertainment.title} />
+					<FAQItem links={appleStore.links} title={appleStore.title} />
+					<FAQItem
+						links={business.links}
+						title={business.title}
+						listHeight90={business.listHeight90}
+					/>
+					<FAQItem
+						links={education.links}
+						title={education.title}
+						listHeight90={education.listHeight90}
+					/>
+					<FAQItem
+						links={healthCare.links}
+						title={healthCare.title}
+						listHeight90={healthCare.listHeight90}
+					/>
+					<FAQItem
+						links={government.links}
+						title={government.title}
+						listHeight90={government.listHeight90}
+					/>
+					<FAQItem links={appleValues.links} title={appleValues.title} />
+					<FAQItem links={aboutApple.links} title={aboutApple.title} />
 				</div>
 			</div>
 			<footer className="mt-10 text-[12px]">
-				<div className="my-hr">
+				<div>
 					<p className="pb-3 text-[#6e6e73]">
 						More ways to shop:
 						<a className="px-1 text-apple-blue" href="#">
@@ -233,14 +165,15 @@ function Footer_accordion() {
 						near you. Or call 1-800-MY-APPLE.
 					</p>
 				</div>
-				<div className="flex items-center justify-between pt-3">
-					<p className="pb-5 text-[#6e6e73]">
+				<div className="flex flex-col items-start justify-between pt-3">
+					<p className="text-md pb-3">Ghana</p>
+
+					<p className="text-[#6e6e73]">
 						Copyright © 2023 Apple Inc. All rights reserved.
 					</p>
-					<p>
+					<p className="text-[#6e6e73]">
 						Privacy | Policy | Terms of Use | Sales and Refunds | Legal Site Map
 					</p>
-					<p>Ghana</p>
 				</div>
 			</footer>
 		</div>
