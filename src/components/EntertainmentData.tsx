07adaa8e9ba26c807dfa3c65b2b78cccb5535794
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:33:11 +0000

    update entertainment

--- src/components/EntertainmentData.tsx
+++ src/components/EntertainmentData.tsx
@@ -1,22 +1,52 @@
-function EntertainmentData({ Navdata }: any) {
+import { Link, useLocation } from "react-router-dom";
+
+interface entertainmentInterface {
+	title: string;
+	data: string[];
+	entertainment_title: string;
+	entertainment_links: string[];
+}
+
+interface entertainmentProps {
+	Navdata: entertainmentInterface;
+}
+
+function EntertainmentData({ Navdata }: entertainmentProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const entertainment_title = Navdata?.entertainment_title;
 	const entertainment_links = Navdata.entertainment_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 
 	return (
 		<div className="my-grid mx-[20px] mt-[150px] h-screen max-w-[400px] md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{entertainment_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{entertainment_links}
 				</ul>
 			</div>
