5dcf2840b09d0fce681495cd70dbc253868cbc22
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:28:56 +0000

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -23,15 +23,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 			<Routes>
 				<Route path="/" element={<App />}></Route>
 				<Route path="/store" element={<Store />}></Route>
-				<Route path="/mac" element={<Mac />}></Route>
+				{/* <Route path="/mac" element={<Mac />}></Route>
 				<Route path="/ipad" element={<Ipad />}></Route>
 				<Route path="/iphone" element={<Iphone />}></Route>
-				<Route path="/watch" element={<Watch />}></Route>
-				<Route path="/airpods" element={<Airpods />}></Route>
-				<Route path="/tv&home" element={<TvHome />}></Route>
-				<Route path="/entertainment" element={<Entertainment />}></Route>
-				<Route path="/accessories" element={<Accessories />}></Route>
-				<Route path="/Support" element={<Support />}></Route>
+				<Route path="/watch" element={<Watch />}></Route> */}
+				{/* <Route path="/airpods" element={<Airpods />}></Route> */}
+				{/* <Route path="/tv&home" element={<TvHome />}></Route> */}
+				{/* <Route path="/entertainment" element={<Entertainment />}></Route> */}
+				{/* <Route path="/accessories" element={<Accessories />}></Route> */}
+				{/* <Route path="/Support" element={<Support />}></Route> */}
 				<Route path="*" element={<PageNotFound />}></Route>
 			</Routes>
 		</Router>
