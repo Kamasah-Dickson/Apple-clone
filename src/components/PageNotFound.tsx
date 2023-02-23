b9146072642897cb27255d5447b91af3c1a0ca47
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:37:53 +0000

    update page not found

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
 	return (
-		<div className="grid h-screen w-full place-content-center text-2xl font-medium text-black">
-			Page Not constructed 🚧
+		<div className="grid h-screen w-full place-content-center text-center ">
+			<h1 className="text-2xl font-medium text-black md:text-4xl">
+				Page Under Construction 🚧😷
+			</h1>
+			<p className="text-md mt-2 font-light md:text-2xl">
+				Project is basically to demonstrate or showcase a skill
+			</p>
+			<Link
+				to="/"
+				className="my-trans mt-6 text-center text-4xl hover:scale-125 active:-scale-125"
+			>
+				👽
+			</Link>
 		</div>
 	);
 }
