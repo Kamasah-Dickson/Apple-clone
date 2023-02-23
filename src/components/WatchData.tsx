e76d8eb335192d697757787081f4c6e36cea9aa4
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:32:12 +0000

    update wath data

--- src/components/WatchData.tsx
+++ src/components/WatchData.tsx
@@ -1,32 +1,70 @@
-function WatchData({ Navdata }: any) {
+import { Link, useLocation } from "react-router-dom";
+
+interface watchInterface {
+	title: string;
+	data: string[];
+	shopWatch_title: string;
+	shopWatch_links: string[];
+	moreWatch_title: string;
+	moreWatch_links: string[];
+}
+
+interface watchProps {
+	Navdata: watchInterface;
+}
+
+function WatchData({ Navdata }: watchProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const shopWatch_title = Navdata?.shopWatch_title;
 	const shopWatch_links = Navdata.shopWatch_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 	const moreWatch_title = Navdata?.moreWatch_title;
 	const moreWatch_links = Navdata.moreWatch_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 
 	return (
-		<div className="my-grid mx-[20px] mt-[150px] h-screen max-w-[750px] md:mt-8">
+		<div className="my-grid3 mx-[20px] mt-[150px] h-screen max-w-[750px] md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[20px] text-[#E8E8ED] md:text-[24px]">
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{shopWatch_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{shopWatch_links}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{moreWatch_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{moreWatch_links}
 				</ul>
 			</div>
