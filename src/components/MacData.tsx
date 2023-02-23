53dfe1476dfe0bf0abdf416ab047009e4c6dbf89
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:29:51 +0000

    update mac data

--- src/components/MacData.tsx
+++ src/components/MacData.tsx
@@ -1,32 +1,69 @@
-function MacData({ Navdata }: any) {
+import { useLocation, Link } from "react-router-dom";
+
+interface macInterface {
+	title: string;
+	data: string[];
+	shopMac_title: string;
+	shopMac_links: string[];
+	moreMac_title: string;
+	moreMac_links: string[];
+}
+interface macProps {
+	Navdata: macInterface;
+}
+
+function MacData({ Navdata }: macProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const shopMac_title = Navdata?.shopMac_title;
 	const shopMac_links = Navdata.shopMac_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 	const moreMac_title = Navdata?.moreMac_title;
 	const moreMac_links = Navdata.moreMac_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 
 	return (
 		<div className="my-grid mx-[20px] mt-[150px] h-screen md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{shopMac_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{shopMac_links}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{moreMac_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{moreMac_links}
 				</ul>
 			</div>
