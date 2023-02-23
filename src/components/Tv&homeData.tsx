3bc35514d73eb3dba5ca339994222180c984d55b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:35:27 +0000

    update tv and home data

--- src/components/Tv&homeData.tsx
+++ src/components/Tv&homeData.tsx
@@ -1,32 +1,69 @@
-function Tv_homeData({ Navdata }: any) {
+import { Link, useLocation } from "react-router-dom";
+
+interface tv_homeInterface {
+	title: string;
+	data: string[];
+	shopTV_title: string;
+	shopTV_links: string[];
+	moreTV_title: string;
+	moreTV_links: string[];
+}
+interface tv_homeProps {
+	Navdata: tv_homeInterface;
+}
+
+function Tv_homeData({ Navdata }: tv_homeProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const shopTV_title = Navdata?.shopTV_title;
 	const shopTV_links = Navdata.shopTV_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 	const moreTV_title = Navdata?.moreTV_title;
 	const moreTV_links = Navdata.moreTV_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 
 	return (
 		<div className="my-grid mx-[20px] mt-[150px] h-screen md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{shopTV_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{shopTV_links}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{moreTV_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{moreTV_links}
 				</ul>
 			</div>
