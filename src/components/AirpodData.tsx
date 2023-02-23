7d8dd5f624d86a4246de7444c751d251e8f9c29b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:34:10 +0000

    update airpod data

--- src/components/AirpodData.tsx
+++ src/components/AirpodData.tsx
@@ -1,32 +1,70 @@
-function AirpodData({ Navdata }: any) {
+import { Link, useLocation } from "react-router-dom";
+
+interface airpodsInterface {
+	title: string;
+	data: string[];
+	shopPods_title: string;
+	shopPods_links: string[];
+	morePods_title: string;
+	morePods_links: string[];
+}
+
+interface airpodsProps {
+	Navdata: airpodsInterface;
+}
+
+function AirpodData({ Navdata }: airpodsProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const shopPods_title = Navdata?.shopPods_title;
 	const shopPods_links = Navdata.shopPods_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 	const morePods_title = Navdata?.morePods_title;
 	const morePods_links = Navdata.morePods_links?.map((data: any) => (
-		<li key={data}>{data}</li>
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
 	));
 
 	return (
-		<div className="my-grid mx-[20px] mt-[150px] h-screen max-w-[400px] md:mt-8">
+		<div className="my-grid3 mx-[20px] mt-[150px] h-screen max-w-[750px] md:mt-8">
 			<div>
 				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{shopPods_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{shopPods_links}
 				</ul>
 			</div>
 			<div className="md:justify-self-end">
 				<h3 className="text-[12px] text-[#86868b]">{morePods_title}</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{morePods_links}
 				</ul>
 			</div>
