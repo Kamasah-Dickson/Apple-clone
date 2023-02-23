fe6a6c2c64b1d89933f7abab630c18c5ee229a97
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-23 03:32:43 +0000

    update accessories data

--- src/components/AccessoriesData.tsx
+++ src/components/AccessoriesData.tsx
@@ -1,16 +1,49 @@
-function AccessoriesData({ Navdata }: any) {
+import { Link, useLocation } from "react-router-dom";
+
+interface accessoriesInterface {
+	title: string;
+	data: string[];
+	explore_accessories_title: string;
+	explore_accessories_links: string[];
+}
+
+interface accessoriesProps {
+	Navdata: accessoriesInterface;
+}
+function AccessoriesData({ Navdata }: accessoriesProps) {
+	const location = useLocation();
+	const headerWhite = location.pathname === "/store";
+
 	const title = Navdata.title;
-	const list = Navdata.data.map((data: any) => <li key={data}>{data}</li>);
+	const list = Navdata.data.map((data: any) => (
+		<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+			<li>{data}</li>
+		</Link>
+	));
 	const explore_accessories_title = Navdata?.explore_accessories_title;
 	const explore_accessories_links = Navdata.explore_accessories_links?.map(
-		(data: any) => <li key={data}>{data}</li>
+		(data: any) => (
+			<Link key={data} to={`/${data}`} className="hover:text-gray-400">
+				<li>{data}</li>
+			</Link>
+		)
 	);
 
 	return (
 		<div className="my-grid mx-[20px] mt-[150px] h-screen max-w-[400px] md:mt-8">
 			<div>
-				<h3 className="text-[12px] text-[#86868b]">{title}</h3>
-				<ul className="mt-4 flex flex-col gap-0 text-[24px] text-[#E8E8ED]">
+				<h3
+					className={`text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#86868b]"
+					}`}
+				>
+					{title}
+				</h3>
+				<ul
+					className={`mt-4 flex flex-col gap-0 text-[24px] ${
+						headerWhite ? "font-semibold text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{list}
 				</ul>
 			</div>
@@ -18,7 +51,11 @@ function AccessoriesData({ Navdata }: any) {
 				<h3 className="text-[12px] text-[#86868b]">
 					{explore_accessories_title}
 				</h3>
-				<ul className="mt-4 flex flex-col gap-1 text-[12px] text-[#E8E8ED]">
+				<ul
+					className={`mt-4 flex flex-col gap-1 text-[12px] ${
+						headerWhite ? "text-grayish" : "text-[#E8E8ED]"
+					}`}
+				>
 					{explore_accessories_links}
 				</ul>
 			</div>
