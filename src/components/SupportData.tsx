857ffdd5be29cc9c222921c76f0a6d8c0e68b3be
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-26 03:12:44 +0000

    update support data

--- src/components/SupportData.tsx
+++ src/components/SupportData.tsx
@@ -15,7 +15,7 @@ interface supportProps {
 
 function SupportData({ Navdata }: supportProps) {
 	const location = useLocation();
-	const headerWhite = location.pathname === "/store";
+	const headerWhite = location.pathname === "/store" || "shop the latest";
 
 	const title = Navdata.title;
 	const list = Navdata.data.map((data: any) => (
