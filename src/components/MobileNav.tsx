00b6a74999085e328652101feac0a88933fb3b9a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-26 03:17:58 +0000

    update mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -26,7 +26,7 @@ function MobileNav({
 }: mobileProp) {
 	const [extraNav, setExtraNav] = useState(false);
 	const location = useLocation();
-	const headerWhite = location.pathname === "/store";
+	const headerWhite = location.pathname === "/store" || "shop the latest";
 
 	return (
 		<div>
