70793fb124c6ea4b063bd5630bcb20df5e9af5a2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-26 03:25:13 +0000

    add types data

---
+++ src/TypesData.ts
@@ -0,0 +1,78 @@
+export type latestProps = {
+	name: string;
+	img: string;
+	para: string;
+	text: string;
+	white: boolean;
+}[];
+
+export type productsProp = {
+	name: string;
+	img: string;
+}[];
+
+export interface titleProp {
+	h1: string;
+	span: string;
+}
+
+export interface storeDifference {
+	img: JSX.Element;
+	text: string;
+	green: boolean;
+}
+[];
+
+// type Accessories ={
+
+// }
+
+export type AccessoriesProp = {
+	img: string;
+	new: boolean;
+	title: string;
+	price: string;
+}[];
+
+// =====================
+export type differenceProp = {
+	img: JSX.Element;
+	text: JSX.Element;
+	green?: boolean;
+	purple?: boolean;
+	blue?: boolean;
+	orange?: boolean;
+	name: string;
+}[];
+
+export type leadProp = {
+	title: string;
+	text: string;
+	img: string;
+}[];
+
+// =====================
+
+export interface defferenceDataProp {
+	difference: differenceProp;
+}
+
+export interface titleProp {
+	h1: string;
+	span: string;
+}
+
+export interface titleType {
+	title: titleProp;
+}
+
+export type headingProp = {
+	span: string;
+	text: string;
+};
+
+export interface productProps {
+	leading: leadProp;
+	product: AccessoriesProp;
+	heading: headingProp;
+}
