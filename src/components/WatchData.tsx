32789e79282eac10cca10800b96136ae1c6cd9d1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-01 01:49:19 +0000

    update watch data

--- src/components/WatchData.tsx
+++ src/components/WatchData.tsx
@@ -15,7 +15,7 @@ interface watchProps {
 
 function WatchData({ Navdata }: watchProps) {
 	const location = useLocation();
-	const headerWhite = location.pathname === "/store" || "shop the latest";
+	const headerWhite = location.pathname === "/store";
 
 	const title = Navdata.title;
 	const list = Navdata.data.map((data: any) => (
