775a48fdb99b38a04f11113b83fa53f70558623b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-01 01:42:16 +0000

    update store

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -2,10 +2,12 @@ import Footer from "./Footer";
 import Store_Products from "./Store_Products";
 import TopAdd from "./TopAdd";
 import Latest_Products from "./Latest_Products";
-import PageNotFound from "./PageNotFound";
 import Help_is_here from "./Help_is_here";
 import Store_difference from "./Store_difference";
 import Accessory_Products from "./Accessory_Products";
+import Final_Products from "./Final_Products";
+import Final_Exclusive from "./Final_Exclusive";
+
 import {
 	Store_Products_Data,
 	Latest_Products_data,
@@ -18,11 +20,15 @@ import {
 	loudAndClearData,
 	LoudClearLead,
 	Accessory_heading,
+	experience_heading,
+	Apple_Experience_data,
+	Exclusive_data,
+	exclusive_heading,
 } from "./StoreProductsData";
 
 function Store() {
 	return (
-		<main className="">
+		<main>
 			<TopAdd />
 			{/* 1170 */}
 			<div className="mx-auto bg-[#f5f5f7]">
@@ -53,11 +59,31 @@ function Store() {
 					leading={LoudClearLead}
 					heading={LoudAndClearTitle}
 				/>
-				<PageNotFound />
 				{/* ===Apple-Experience=== */}
-				<div>{/* <h1>Store</h1> */}</div>
-				{/* ===special-stores and QuickLinks=== */}
-				<div>{/* <h1>Store</h1> */}</div>
+				<Final_Products
+					product={Apple_Experience_data}
+					heading={experience_heading}
+				/>
+				{/* ===special-stores ===*/}
+				<Final_Exclusive product={Exclusive_data} heading={exclusive_heading} />
+				{/* ===Quick_Links=== */}
+				<div className="mx-auto max-w-[920px] pb-7">
+					<h1 className="mb-3 mt-10 text-3xl font-semibold">Quick Links</h1>
+					<div className=" flex flex-wrap items-center justify-start gap-5">
+						<a className="rounded-3xl bg-gray-200 py-2 px-3" href="#">
+							Order Status
+						</a>
+						<a className="rounded-3xl bg-gray-200 py-2 px-3" href="#">
+							Shopping help
+						</a>
+						<a className="rounded-3xl bg-gray-200 py-2 px-3" href="#">
+							Returns
+						</a>
+						<a className="rounded-3xl bg-gray-200 py-2 px-3" href="#">
+							Your Saves
+						</a>
+					</div>
+				</div>
 			</div>
 			<Footer />
 		</main>
