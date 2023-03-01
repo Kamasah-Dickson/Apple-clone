9bf4ba6157fb4ffc558a5b2074e5acf1a0f70bed
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-01 01:41:47 +0000

    update store  products

--- src/components/StoreData.tsx
+++ src/components/StoreData.tsx
@@ -16,7 +16,7 @@ interface storeData {
 
 function StoreData({ Navdata }: storeData) {
 	const location = useLocation();
-	const headerWhite = location.pathname === "/store" || "shop the latest";
+	const headerWhite = location.pathname === "/store";
 
 	const title = Navdata.title;
 	const list = Navdata.data.map((data) => (
